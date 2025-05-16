c72bf5936410032616ffa4ea2236b175374f5161
Sarthak Gadge <sarthakgadge43@gmail.com>
2025-05-16 21:50:19 +0530

    added lazygit via snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,11 +1,38 @@
--- {
---   "folke/snacks.nvim",
---   ---@type snacks.Config
---   opts = {
---     lazygit = {
---       -- your lazygit configuration comes here
---       -- or leave it empty to use the default settings
---       -- refer to the configuration section below
---     }
---   }
--- }
+return {
+  "folke/snacks.nvim",
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    lazygit = {
+      configure = true,
+      config = {
+        os = { editPreset = "nvim-remote" },
+        gui = {
+          nerdFontsVersion = "3", -- set to "" to disable icons
+        },
+      },
+      theme_path = vim.fs.normalize(vim.fn.stdpath("cache") .. "/lazygit-theme.yml"),
+      theme = {
+        [241]                      = { fg = "Special" },
+        activeBorderColor          = { fg = "MatchParen", bold = true },
+        cherryPickedCommitBgColor  = { fg = "Identifier" },
+        cherryPickedCommitFgColor  = { fg = "Function" },
+        defaultFgColor             = { fg = "Normal" },
+        inactiveBorderColor        = { fg = "FloatBorder" },
+        optionsTextColor           = { fg = "Function" },
+        searchingActiveBorderColor = { fg = "MatchParen", bold = true },
+        selectedLineBgColor        = { bg = "Visual" },
+        unstagedChangesColor       = { fg = "DiagnosticError" },
+      },
+      win = {
+        style = "lazygit",
+      },
+    },
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>gg", function()
+      require("snacks.terminal").open("lazygit")
+    end, { desc = "Open LazyGit (snacks)" })
+  end,
+}
+
