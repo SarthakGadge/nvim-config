4a5044a71f5fb9b08633b04c46f89cbc0dbf1cd7
Sarthak Gadge <sarthakgadge43@gmail.com>
2025-05-16 22:18:31 +0530

    added snacks notifications

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -28,6 +28,30 @@ return {
         style = "lazygit",
       },
     },
+    notifier = {
+      timeout = 3000,
+      width = { min = 40, max = 0.4 },
+      height = { min = 1, max = 0.6 },
+      margin = { top = 0, right = 1, bottom = 0 },
+      padding = true,
+      sort = { "level", "added" },
+      level = vim.log.levels.TRACE,
+      icons = {
+        error = " ",
+        warn  = " ",
+        info  = " ",
+        debug = " ",
+        trace = " ",
+      },
+      keep = function(notif)
+        return vim.fn.getcmdpos() > 0
+      end,
+      style = "compact",
+      top_down = true,
+      date_format = "%R",
+      more_format = " ↓ %d lines ",
+      refresh = 50,
+    }
   },
   config = function()
     vim.keymap.set("n", "<leader>gg", function()
